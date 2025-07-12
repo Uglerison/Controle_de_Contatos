1a16d9e2179ab55d1b1e01c63d9ae63bdc87f81f
Uglerison <uglerisonjabes@gmail.com>
2025-07-11 22:27:01 -0300

    Finished users CRUD

--- Contatos/Controllers/Usuarios.cs
+++ Contatos/Controllers/Usuarios.cs
@@ -6,20 +6,52 @@ namespace Contatos.Controllers
 {
     public class Usuarios : Controller
     {
-            private readonly IUsuarioRepositorio _usuarioRepositorio;
-            public Usuarios(IUsuarioRepositorio usuarioRepositorio)
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public Usuarios(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            List<UsuarioModel> usuarios = _usuarioRepositorio.BuscarTodos();
+            return View(usuarios);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar(int id)
+        {
+            UsuarioModel usuario = _usuarioRepositorio.ListarPorId(id);
+            return View(usuario);
+        }
+
+        public IActionResult ConfirmarExclusao(int id)
+        {
+            UsuarioModel usuario = _usuarioRepositorio.ListarPorId(id);
+            return View(usuario);
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            try
             {
-                _usuarioRepositorio = usuarioRepositorio;
+                _usuarioRepositorio.Apagar(id);
+                TempData["okMsg"] = "Usuario deletado com sucesso";
+                return RedirectToAction("Index");
+
             }
-            public IActionResult Index()
+            catch (Exception erro)
             {
-                List<UsuarioModel> usuarios = _usuarioRepositorio.BuscarTodos();
-                return View(usuarios);
+                TempData["erroMsg"] = $"Usuario não deletado! {erro.Message}";
+                return RedirectToAction("Index");
             }
-            public IActionResult Criar()
-            {
-                return View();
-            }
+        }
+
         [HttpPost]
         public IActionResult Criar(UsuarioModel usuario)
         {
@@ -40,6 +72,26 @@ namespace Contatos.Controllers
                 return RedirectToAction("Index");
             }
         }
-    }   
-}
+
+        [HttpPost]
+        public IActionResult Alterar(UsuarioModel usuario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.Atualizar(usuario);
+                    TempData["okMsg"] = "Usuario editado com sucesso";
+                    return RedirectToAction("Index");
+                }
+                return View("Editar", usuario);
+            }
+            catch (Exception erro)
+            {
+                TempData["erroMsg"] = $"Usuario não editado! {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+    }
 
